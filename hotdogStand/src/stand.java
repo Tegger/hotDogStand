5deca80846c7da26a802a3ba6d0b6f9bb6102ff5
Tegger <42648199+Tegger@users.noreply.github.com>
2018-09-09 12:16:01 -0500

    Add files via upload

---
+++ hotdogStand/src/stand.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.security.*;
+public class stand {
+
+    public static void main(String[] args) {
+
+        String[] hotDogStand = new String[5];
+        hotDogStand[0] = "Bun";
+        hotDogStand[1] = "Hot Dog";
+        hotDogStand[3] = "Relish";
+        hotDogStand[4] = "Onion";
+        hotDogStand[2] = "Mustard";
+
+        for (int i = 0; i < 4; i++){
+            System.out.println(hotDogStand[i]);
+        }
+    }
+
+}
